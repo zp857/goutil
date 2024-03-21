d1125658de2ed5dfc4ab8cf052c68be00d09626e
gaoqianyang <gaoqianyang>
2024-03-21 17:16:39 +0800

    feat(threading): WatchGoroutines

--- threading/routines.go
+++ threading/routines.go
@@ -2,6 +2,9 @@ package threading
 
 import (
 	"github.com/zp857/goutil/rescue"
+	"go.uber.org/zap"
+	"runtime"
+	"time"
 )
 
 func GoSafe(fn func()) {
@@ -11,4 +14,12 @@ func GoSafe(fn func()) {
 func RunSafe(fn func()) {
 	defer rescue.Recover()
 	fn()
+}
+
+func WatchGoroutines(ticker int) {
+	tk := time.NewTicker(time.Duration(ticker) * time.Second)
+	for {
+		zap.L().Sugar().Debug("Current goroutine number: %v", runtime.NumGoroutine())
+		<-tk.C
+	}
 }
